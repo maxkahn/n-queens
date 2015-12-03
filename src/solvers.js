ddf3136dbc6dd60d1f407264f8c04805e67b08e7
maxkahn <kahn.max@gmail.com>
2015-12-02 17:37:32 -0800

    working on Rook solutions, getting bad solutions

--- src/solvers.js
+++ src/solvers.js
@@ -13,13 +13,68 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
+//this.hasAnyRooksConflicts()
+//viable spaces
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var solution = undefined; //fixme
+  var board = new Board({n: n});
+  var rooks = n;
+  var solutions = [];
+  //this.togglePiece(rowI, colI)
+  var possibleMoves = [];
+  for (var row = 0; row < n; row++) {
+    for (var column = 0; column < n; column++) {
+      possibleMoves.push([row, column]);
+    }
+  }
+  
+  // for(0..<n for rows)
+  //   for(0..<n for col)
+  //     possible moves[]
+    var generateBoards = function(board, remainingRooks){
+      if(remainingRooks === 0) {
+        //no check yet for whether board already in solutions
+        solutions.push(board);
+      }
+      //pass into solutions
+
+      //if bad board dont recurse
+      if (!(board.hasAnyRooksConflicts())) {
+        for (var i = 0; i < possibleMoves.length; i++) {
+          if (board.valueAt(possibleMoves[i][0], possibleMoves[i][1]) === 0) {
+            
+            console.table("in generateBoards",board.rows())
+            var newRows = [];
+            var rows = board.rows();
+            for(var subI =0; subI < rows.length; subI++){
+              var newEntry = rows[subI].slice();
+              newRows.push(newEntry);
+            }
+//            console.table(newRows)
+            var newBoard = new Board(newRows);
+            newBoard.togglePiece(possibleMoves[i][0],possibleMoves[i][1]);
+            generateBoards(newBoard, remainingRooks-1);
+          }
+        }
+      }
+    };
+    
+    console.log(board.rows())
+    generateBoards(board, n);
+    //test with this.hasAnyRooksConflicts()
+    //if not any conflicts
+      //iterate over possibleMoves
+        //check that space is blank
+          //if so, toggle it
+          //call recurisvly
+          //pass in that board, remainingRooks--, possibleMove
+
+
+  console.table(solutions[0].rows())
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions));
+  return solutions[0].rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
@@ -45,3 +100,4 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
